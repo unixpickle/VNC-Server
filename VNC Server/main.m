a23d236e1c01aea141a42344eab15b54754a2d06
Alex Nichol <alexnichol@comcast.net>
2012-11-03 14:32:56 -0400

    Initial Commit

---
+++ VNC Server/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  VNC Server
+//
+//  Created by Alex Nichol on 11/3/12.
+//  Copyright (c) 2012 Alex Nichol. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
